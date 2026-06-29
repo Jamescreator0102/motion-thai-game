3e5be354e13762b77100ecf6bdb72b1c1dcd9f1b
Jamescreator0102 <krucreator@gmail.com>
2026-06-29 08:05:54 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,42 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwaBShIUYJpAwyPZxyEUH2Ii-4tH9pP8dySPBuc4v0h9QYID-o_3lRK54yNv9Z_3-Ji/exec";
 
+function apiJSONP(action, params = {}) {
+  return new Promise((resolve, reject) => {
+    const callbackName = "jsonp_callback_" + Date.now() + "_" + Math.floor(Math.random() * 10000);
+
+    window[callbackName] = function (data) {
+      resolve(data);
+      delete window[callbackName];
+      script.remove();
+    };
+
+    const query = new URLSearchParams({
+      action: action,
+      callback: callbackName,
+      ...params
+    });
+
+    const script = document.createElement("script");
+    script.src = API_URL + "?" + query.toString();
+
+    script.onerror = function () {
+      reject(new Error("เชื่อมต่อ API ไม่สำเร็จ"));
+      delete window[callbackName];
+      script.remove();
+    };
+
+    document.body.appendChild(script);
+  });
+}
+
+async function getStudentsFromAPI() {
+  return await apiJSONP("getStudents");
+}
+
+async function getQuestionsFromAPI() {
+  return await apiJSONP("getQuestions");
+}
+
+async function saveScoreToAPI(scoreData) {
+  return await apiJSONP("saveScore", scoreData);
+}
